2454e1e84e8532773b43eaa5aa6b69f7def89f65
Fabian Ordonez <nel.fabi.1010@udenar.edu.co>
2024-05-25 12:21:00 -0500

    Ultimas modificaciones de sidebar en _main_

--- giro/settings.py
+++ giro/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+     # General use templates & template tags (should appear first)
+    'adminlte3',
+     # Optional: Django admin theme (must be before django.contrib.admin)
+    'adminlte3_theme',
+    
     'album', #El que acabo de crear
+    
 ]
 
 MIDDLEWARE = [
